6696c2593ba6597a71fc8d813176e2a7117420f6
Indraneel Purohit <i.m.indraneel@gmail.com>
2015-09-16 23:41:05 -0400

    functional script. ready to run

--- fetch.py
+++ fetch.py
@@ -1,9 +1,13 @@
+import datetime
+import timeit
 import sys
 import json
 import random
 import requests
 import psycopg2
 import config
+
+start_time = timeit.default_timer()
 
 base_url_parts = ["http://app.njtransit.com/NJTAppWS/services/getDV?_dc=","1000000000000","&station=","NY","&callback="]
 stops = ["AM","AB","AZ","AH","AS","AN","AP","AO","AC","AV","BI","BH","MC","BS","BY","BV","BM","BN","BK","BB","BU","BW","BF","CB","CM","CY","IF","CN","XC","DL","DV","DO","DN","EO","ED","EH","EL","EZ","EN","EX","FW","FH","GD","GW","GI","GL","GG","GK","RS","HQ","HL","HN","RM","HW","HZ","HG","HI","HD","UF","HB","JA","KG","HP","ON","LP","LI","LW","FA","LS","LB","LN","LY","MA","MZ","SQ","MW","MP","MU","MI","MD","MB","GO","MK","HS","UV","ZM","MX","MR","HV","OL","TB","MS","ML","MT","MV","MH","NN","NT","NE","NH","NB","NV","NY","NA","ND","NP","OR","NZ","OD","OG","OS","PV","PS","RN","PC","PQ","PN","PE","PH","PF","PL","PP","PO","PR","PJ","FZ","RH","RY","17","RA","RB","RW","RG","RL","RF","CW","TS","SE","RT","XG","SM","CH","SO","LA","SV","SG","SF","ST","TE","TO","TR","TC","US","UM","WK","WA","WG","WT","23","WF","WW","WH","WR","WB","WL"]
@@ -39,7 +43,12 @@ insert_into_default = ["train_line", "station_name", "station_abbr", "destinatio
 
 insert_into = ["sched_dep_day", "sched_dep_time", "track", "line", "train_line", "train_number", "status", "sec_late", "gps_lat", "gps_lon", "gps_time", "station_name", "station_abbr", "destination"]
 
+num_success = 0
+num_error = 0
+stops_count = 0
 for s in stops:
+    stops_count += 1	
+    print "stop",stops_count,"/",len(stops)
     default_train = {
 	    "ITEM_INDEX": "",
 	    "SCHED_DEP_DAY": "1/1/1970",
@@ -61,7 +70,7 @@ for s in stops:
     base_url_parts[1] = str(random.randint(10**12, 10**13-1))
     base_url_parts[3] = s
     req_url = "".join(base_url_parts)
-    print "GET/\t",req_url
+    # print "GET/\t",req_url
     r = requests.get(req_url)
     """
     text = r.text
@@ -69,32 +78,35 @@ for s in stops:
 	continue
     """
     response = json.loads(r.text[1:-1], object_hook=_decode_dict)['STATION']
-    print response
+    # print response
     if not response:
+	num_error += 1
 	continue
     # response = (r.text[1:-1])
     # print response
     con = psycopg2.connect("dbname='"+config.dbname+"' user='"+config.dbuser+"'") 
     cur = con.cursor()
-    print "about to write to db\n"
+    # print "about to write to db\n"
     # sys.exit(1)
     if response['ITEMS'] is None:
-	default_train['STATION_NAME'] = response['STATION_2CHAR']
-	default_train['STATION_ABBR'] = response['STATIONNAME'] 
+	default_train['STATION_ABBR'] = response['STATION_2CHAR']
+	default_train['STATION_NAME'] = response['STATIONNAME'] 
 	try:
 	    cur.execute("INSERT INTO trains("+",".join(insert_into_default)+"""
 	    ) VALUES(%(TRAIN_LINE)s, %(STATION_NAME)s, %(STATION_ABBR)s, %(DESTINATION)s)
 	    """, default_train)
 
 	    con.commit()
-	    print "commited\t",default_train
-
+	    # print "commited\t",default_train
+	    num_success += 1
+	    continue 
 	except Exception, e:
+	    num_error += 1
 	    if con:
 		con.rollback()
-		print "1 - rollback on\t",response
-		print e,"\n"
-		sys.exit(1)
+		with open("errors.txt", "a") as errorlog:
+		    errorlog.write(str(datetime.datetime.now()) + "\t| default |\t",response,"\t|\t",e,"\n")
+
 	
     if isinstance(response['ITEMS']['ITEM'], dict):
 	try:
@@ -105,21 +117,21 @@ for s in stops:
 	    temp_item['SCHED_DEP_TIME'] = response['ITEMS']['ITEM']['SCHED_DEP_DATE'].split(" ")[0]
 	    temp_item['STATION_ABBR'] = response['STATION_2CHAR']
 	    temp_item['STATION_NAME'] = response['STATIONNAME'] 
-	    if "GPSLATITUDE" not in item:
+	    if "GPSLATITUDE" not in temp_item:
 		temp_item['GPSLATITUDE'] = "0.0"
 	    else:
-		if item['GPSLATITUDE'] is "":
+		if temp_item['GPSLATITUDE'] is "":
 		    temp_item['GPSLATITUDE'] = "0.0"
 
-	    if "GPSLONGITUDE" not in item:
+	    if "GPSLONGITUDE" not in temp_item:
 		temp_item['GPSLONGITUDE'] = "0.0"
 	    else:
-		if item['GPSLONGITUDE'] is "":
+		if temp_item['GPSLONGITUDE'] is "":
 		    temp_item['GPSLONGITUDE'] = "0.0"
-	    if "GPSTIME" not in item:
+	    if "GPSTIME" not in temp_item:
 		temp_item['GPSTIME'] = "1/1/1970 00:00:01 AM"
 	    else:
-		if item['GPSTIME'] is "":
+		if temp_item['GPSTIME'] is "":
 		    temp_item['GPSTIME'] = "1/1/1970 00:00:01 AM"
 
 	    cur.execute("INSERT INTO trains("+",".join(insert_into)+"""
@@ -127,14 +139,18 @@ for s in stops:
 	    """, temp_item)
 
 	    con.commit()
-	    print "commited\t",temp_item
+	    # print "commited\t",temp_item
+	    num_success += 1
+	    continue
 # i have to write a thing where i iterate over the item from the API and if theres a non empty string then i can overwrite the original data object but otherwise just use the default original data object 
 	except Exception, e:
+	    num_error += 1
 	    if con:
 		con.rollback()
-		print "2 - rollback on\t",temp_item
-		print e
-		sys.exit(1)
+		with open("errors.txt", "a") as errorlog:
+		    errorlog.write(str(datetime.datetime.now()) + "\t| dict |\t",temp_item,"\t|\t",e,"\n")
+
+
     if isinstance(response['ITEMS']['ITEM'], list):
 	for item in response['ITEMS']['ITEM']:
 	    temp_item = item
@@ -167,14 +183,27 @@ for s in stops:
 		""", temp_item)
 
 		con.commit()
-		print "commited\t",temp_item
-
+		# print "commited\t",temp_item
+		num_success += 1
+		continue 
 	    except Exception, e:
+		num_error += 1
 		if con:
 		    con.rollback()
-		    print "3 - rollback on\t",temp_item
-		    print e,"\n"
-		    sys.exit(1)
+		    with open("errors.txt", "a") as errorlog:
+			errorlog.write(str(datetime.datetime.now()) + "\t| list |\t",temp_item,"\t|\t",e,"\n")
 
-		# sys.exit(1)
 
+
+
+
+stop_time = timeit.default_timer()
+
+ingestion_stats = {"successes" : num_success, "errors" : num_error }
+try:
+    cur.execute("INSERT INTO ingestion_stats(successes, errors) VALUES(%(successes)s, %(errors)s)", ingestion_stats)
+    con.commit()
+except Exception, e:
+    with open("stat_error.txt", "a") as staterrorlog:
+	staterrorlog.write(str(datetime.datetime.now()) + "\t| stats |\t",str(ingestion_stats),"\t|\t",e,"\n")
+    
